f0c49fb66c6a7f7eb8c85f8bf0076ee4bee10b3a
Michael Rideout <rideout.m@hotmail.com>
2022-09-27 22:07:58 -0400

    fix footer paddings for mobi

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,21 @@ function Footer() {
     <div className="footer">
       <h4 className="footer__title">Annabelle</h4>
       <div className="footer__socials">
-        <a href="#">Facebook</a> | <a href="#">Instagram</a> |
-        <a href="#">Twitter</a>
+        <a className="footer__socials-link" href="#">
+          Facebook
+        </a>{" "}
+        |{" "}
+        <a className="footer__socials-link" href="#">
+          Instagram
+        </a>{" "}
+        |
+        <a className="footer__socials-link" href="#">
+          Twitter
+        </a>
       </div>
-      <p>Restaurant Annabelle &copy; 2020, All Rights Reserved.</p>
+      <p className="footer__details">
+        Restaurant Annabelle &copy; 2020, All Rights Reserved.
+      </p>
     </div>
   );
 }
